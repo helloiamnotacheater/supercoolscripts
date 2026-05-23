38c71252f402e155f0313c5e1a4a7ae41c358f8e
blablablablebleblebloobloobloo <co.co.nut.help.roblox@gmail.com>
2026-05-23 20:57:16 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,7 @@
+local gameId = game.PlaceId
+
+if gameId == 114234929420007 then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/helloiamnotacheater/supercoolscripts/refs/heads/main/BloxStrikeHubFREE.lua')()
+elseif gameId == 17625359962 then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/helloiamnotacheater/supercoolscripts/refs/heads/main/KidzHubRivalsFREE.lua')()
+end
