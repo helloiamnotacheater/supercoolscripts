270a27ca4ce8986cac1e88c780aeb1f2b2b56b83
blablablablebleblebloobloobloo <co.co.nut.help.roblox@gmail.com>
2026-05-24 10:56:30 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,6 @@ if gameId == 114234929420007 then
     loadstring(game:HttpGet'https://raw.githubusercontent.com/helloiamnotacheater/supercoolscripts/refs/heads/main/BloxStrikeHubFREE.lua')()
 elseif gameId == 17625359962 then
     loadstring(game:HttpGet'https://raw.githubusercontent.com/helloiamnotacheater/supercoolscripts/refs/heads/main/KidzHubRivalsFREE.lua')()
+elseif gameId == 142823291 then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/helloiamnotacheater/supercoolscripts/refs/heads/main/KidzHubMM2FREE.lua')()
 end
